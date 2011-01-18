873a37c692d3f23f4cd28fb9dffd270939dba611
nvasilakis <nikos.ailo@gmail.com>
2011-01-18 18:07:38 +0200

    Optimizing tree.rb

--- tree.rb
+++ tree.rb
@@ -9,48 +9,80 @@ require 'rubygems' # for dbf gem
 require 'segment'
 require 'dbf'      # gem install dbf
 
-def recalc(subtree)
-  fullCount = 0
-  subtree.each do |node|
-    unless node.normalize==1
-      # if not leaf
-      recalc(holon.searchFor(node).listOfChildren)
-    end
-    # uplogizw to sum of coun and re-normalize
-    fullCount = fullCount + node.weight
+class Tree
 
+  @structure
+
+  def initialize()
+    @structure = []
   end
-  if node.is_leaf == 1
-    node.
-  end  
-end
-records = []
-file1 = DBF::Table.new("input.dbf")
-file2 = DBF::Table.new("input.dbf")
-file1.each do |record|
-  row_rec = Segment.new(record.attributes["COUNT_coun"]) #note: we represent COUNT_coun as weight
-  row_rec.x_start=record.attributes["x_start"]
-  row_rec.y_start=record.attributes["y_start"]
-  row_rec.x_end=record.attributes["x_end"]
-  row_rec.y_end=record.attributes["y_end"]
-  row_rec.length=record.attributes["length"]
-  row_rec.is_leaf=record.attributes["is_leave"] #note: is_leaf is erroneously represented as is_leave
-  row_rec.edge_id=record.attributes["edge_id"]
-  #second Iteration to find father
-  file2.each do |record2|
-    #  end(i) = start(parent(i))
-    if record2.attributes["x_start"]==record.attributes["x_end"]
-      row_rec.parent=record2.attributes["edge_id"]
-    end
-    # start(i) = end(children(i))
-    if record2.attributes["x_end"]==record.attributes["x_start"]
-      row_rec.addChild(record2.attributes["edge_id"])
+
+  def load(file)
+    copy1 = DBF::Table.new(file)
+    copy1.each do |record|
+      row_rec = Segment.new(record.attributes["COUNT_coun"]) #note: we represent COUNT_coun as weight
+      row_rec.x_start=record.attributes["x_start"]
+      row_rec.y_start=record.attributes["y_start"]
+      row_rec.x_end=record.attributes["x_end"]
+      row_rec.y_end=record.attributes["y_end"]
+      row_rec.length=record.attributes["length"]
+      row_rec.is_leaf=record.attributes["is_leave"] #note: is_leaf is erroneously represented as is_leave
+      row_rec.edge_id=record.attributes["edge_id"]
+      puts "[debug] [load] row edge id: #{row_rec.edge_id}"
+      @structure.push(row_rec)
     end
   end
-  records.push(row_rec)
-  
+
+  def build
+    str_copy = @structure.dup
+    #second Iteration to find father
+    @structure.each do |segment|
+      str_copy.each do |segment_copy|
+        #  end(i) = start(parent(i))
+        if segment_copy.x_start==segment.x_end
+          segment.parent=segment_copy.edge_id
+        end
+        # start(i) = end(children(i))
+        if segment_copy.x_end==segment.x_start
+          segment.addChild(segment_copy.edge_id)
+        end
+      end
+      puts "[debug] [build] row edge id: #{segment.edge_id}"
+    end
+  end
+
+  def recalc(subtree)
+    fullCount = 0
+    subtree.each do |node|
+      unless node.normalize==1
+        # if not leaf
+        recalc(holon.searchFor(node).listOfChildren)
+      else  
+        node.normalize
+      end
+      # uplogizw to sum of coun and re-normalize
+      fullCount = fullCount + node.weight
+    end
+  end
+
+  def get(index)
+    puts "[debug] index: #{index}"
+    segment = @structure[index]
+    return segment
+  end
 end
 
-puts " row 1 count: #{records[0].weight} and edge id: #{records[0].edge_id}"
+
+    #if (node.is_leaf == 1)
+      #node.
+    #end  
+
+holon = Tree.new
+holon.load("input.dbf")
+holon.build
+#holon.
+seg = holon.get(0)
+puts " row 1 count: #{seg.class} and edge id: #{seg.class}"
+#puts " row 1 count: #{records[0].weight} and edge id: #{records[0].edge_id}"
 
 
