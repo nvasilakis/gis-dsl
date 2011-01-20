d3e376d77197445239d5c9e5af263c0a8fe036d5
nvasilakis <nikos.ailo@gmail.com>
2011-01-20 21:55:29 +0200

    Updating calculation to use tree root

--- calc_script.rb
+++ calc_script.rb
@@ -14,12 +14,19 @@ require "tree"
 holon = Tree.new
 holon.load("input.dbf")
 holon.build
+puts "--------"
+root = holon.getRoot
+puts "--------"
 
 puts " row 0 count: #{holon.get(0).weight} and edge id: #{holon.get(0).edge_id}"
 puts " row 1 count: #{holon.get(1).weight} and edge id: #{holon.get(1).edge_id}"
 #puts " row 1 count: #{records[0].weight} and edge id: #{records[0].edge_id}"
 
-holon.recalc(holon.get(1))
+holon.recalc(root)
 puts " row 0 count: #{holon.get(0).weight} and edge id: #{holon.get(0).edge_id}"
 puts " row 1 count: #{holon.get(1).weight} and edge id: #{holon.get(1).edge_id}"
+puts "--------"
+holon.getRoot
+puts "--------"
+
 
